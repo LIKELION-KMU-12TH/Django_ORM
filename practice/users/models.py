6d4c7ae5772207ec4d407e704013676c78eccbe6
dani0910 <dlekdms0910@naver.com>
2024-04-14 22:55:14 +0900

    day8

--- practice/users/models.py
+++ practice/users/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+#장고에서 user모델을 제공해줌
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    nickname = models.CharField(max_length=10, null=True)
+    image = models.ImageField(upload_to='profile/')
+    #null=True가 안되어있을 때 오류가 나는 것이 많아서 실습을 진행하기 위해 넣어준것 뿐임
+    #실제 프로젝트 때는 null을 남발하는 것은 좋지 않음 
+
+class Meta:
+    db_table = 'profile' 
+    #메타 클래스는 장고가 알아서 생성해주는 db인데 이름이 지맘대로 설정됨 
+    #따라서 profile이라는 이름으로 설정해줘라 라고 지정해준것
+
+
+def _str_(self):
+    return self.nickname
+#출력시켰을 때 nickname이 return 되도록 해줌
--- practice/users/views.py
+++ practice/users/views.py
@@ -1,2 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from .models import Profile
+from django.contrib import auth
 
+#auth가 들어가는건 다 인증하는 것
+#장고는 회원가입,로그인 등을 기능을 간단하게 제공해줌
+
+#회원가입
+def signup(request):
+    if request.method == "POST":
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'],
+                email = request.POST['email'],
+            )
+            
+            profile = Profile(
+                user=user,
+                nickname=request.POST['nickname'],
+                image=request.FILES.get('profile_image'),
+            )
+
+            profile.save() #장고에서 제공되는 것 말고 우리가 만든 모델은 save를 해줘야 db에 반영이됨
+            auth.login(request, user)
+            return redirect('home')
+        
+        return render(request, 'signup.html')
+    return render(request, 'signup.html')
+
+#로그인
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        return render(request, 'login.html')
+    return render(request, 'login.html')
+
+#로그아웃
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
